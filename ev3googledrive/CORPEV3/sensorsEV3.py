99856957801e3b84aeafb32663102861fb7dc229
DreVinciBot <35714752+DreVinciBot@users.noreply.github.com>
2020-07-25 21:14:10 -0400

    Added mqtt method of publish/subscriber communication

--- ev3googledrive/CORPEV3/sensorsEV3.py
+++ ev3googledrive/CORPEV3/sensorsEV3.py
@@ -2,6 +2,22 @@
 from ev3dev.ev3 import *
 from time import sleep
 
+import paho.mqtt.client as mqtt
+
+def publishToUnity(sensor_type, sensor_value):
+        broker = "broker.hivemq.com"
+        MQTT_Broker = broker
+
+        client = mqtt.Client()
+        client.connect(MQTT_Broker,1883,60)
+        print("Connected with local")
+
+        # temp = [str(sensor_type), str(sensor_value)]
+        payload = "{sensor}:{value}".format(sensor = sensor_type, value = sensor_value)
+        print(payload)
+        client.publish("topic/EV3ARProject", payload)
+        # client.disconnect();
+
 def read_sensor(sensor):
         print (sensor)
         print (sensor['name'])
@@ -23,6 +39,10 @@ def set_sensor(content):
                 if(type=='Touch'):
                         port='in'+str(content['port'])
                         ts = TouchSensor(port)
+                        print("touch sensor: " + str(ts.value()))
+                        
+                        publishToUnity(type, ts.value())
+
                         return ts.value()
                 if(type=='Color'):
                         port='in'+str(content['port'])
@@ -32,23 +52,43 @@ def set_sensor(content):
                                 rgb_raw.append(cl.value(0))
                                 rgb_raw.append(cl.value(1))
                                 rgb_raw.append(cl.value(2))
+                                print("Color Raw: " + str(rgb_raw))
+                                
+                                publishToUnity(type, rgb_raw)
+
                                 return rgb_raw
                         else:
+                                print("Color sensor: " + str(c1.value()))
+                                
+                                publishToUnity(type, str(c1.value()))
+
                                 return cl.value()
                 if(type=='Gyro'):
                         port='in'+str(content['port'])
                         gy = GyroSensor(port)
                         gy.mode=content['modeId']
+                        print("Gryo sensor: " + str(gy.value()))
+                        
+                        publishToUnity(type, gy.value())
+
                         return gy.value()
                 if(type=='Ultrasonic'):
                         port='in'+str(content['port'])
                         us=UltrasonicSensor(port)
                         us.mode=content['modeId']
+                        print("Ultrasonic sensor: " + str(us.value()))
+
+                        publishToUnity(type, us.value())
+
                         return us.value()
                 if(type=='Infrared'):
                         port='in'+str(content['port'])
                         ir = InfraredSensor(port)
                         ir.mode=content['modeId']
+                        print("Infrared sensor: " + str(ir.value()))
+
+                        publishToUnity(type, ir.value())
+
                         return ir.value()
         except:
                 return ("ko")
@@ -75,6 +115,7 @@ class SensorTypeSwitch:
                 return ts.value()
         def case_color(self):
                 cl = ColorSensor()
+                print(c1, c1.value())
                 return cl.value()
         def case_gyro(self):
                 gy = GyroSensor()
